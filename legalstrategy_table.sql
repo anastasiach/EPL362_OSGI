c55975cb27ed3cdf2e4ef3db7f1bdf1ae650c08b
mpashi01 <mpashi01@cs.ucy.ac.cy>
2018-04-02 15:19:51 +0300

    Add files via upload

---
+++ legalstrategy_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `legalstrategy` (
+  `LegalStrategyID` int(11) NOT NULL,
+  `Name` varchar(45) NOT NULL,
+  `Side-effects` varchar(1000) NOT NULL,
+  PRIMARY KEY (`LegalStrategyID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
