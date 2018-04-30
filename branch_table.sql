72472fd41f70dbfd721f425491c8666244856265
anastasiach <acheim01@cs.ucy.ac.cy>
2018-04-30 18:27:19 +0300

    Delete branch_table.sql

--- branch_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `branch` (
-  `BranchID` int(11) NOT NULL,
-  `Name` varchar(45) NOT NULL,
-  `Address` varchar(45) NOT NULL,
-  `City` varchar(45) NOT NULL,
-  `DropIn` bit(1) NOT NULL,
-  `PreArranged` bit(1) NOT NULL,
-  PRIMARY KEY (`BranchID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
