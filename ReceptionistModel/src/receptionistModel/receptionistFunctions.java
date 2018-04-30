dcf74985d8959996600ee6ee7fbcb9ea01e31bb6
mpashi01 <mpashi01@cs.ucy.ac.cy>
2018-04-30 18:34:21 +0300

    Update receptionistFunctions.java

--- ReceptionistModel/src/receptionistModel/receptionistFunctions.java
+++ ReceptionistModel/src/receptionistModel/receptionistFunctions.java
@@ -2,6 +2,13 @@ package receptionistModel;
 
 import java.sql.ResultSet;
 
+/**
+ * This is the interface of the receptionistService
+ * 
+ * @author Anastasia, Anastasia, Antonia, Marina
+ *
+ */
+
 public interface receptionistFunctions {
 	
 	ResultSet getClients();
@@ -12,17 +19,22 @@ public interface receptionistFunctions {
 	
 	ResultSet getInfoForClient(int id);
 	
-	String getCliniName(int id);
+	String getBranchName(int id);
 	
 	boolean creareNewRandevou(int cid, String date, int time, String branch, int sid, int drop, int cancel, int update, int append);
 	
-	String[] getLastConditionAndMedicationOfPatient(int patient_id);
+	String[][] getClientRecord(int client_id);
+	
+    String[][] getRL(int client_id);
 	
 	ResultSet getRandevous ();
 	
 	String getLawyerName(int did);
 	
 	 boolean attendRandevou(int rid);
+	 
+	 ResultSet getRL1(int client_id);
+	
+	 ResultSet getClientRecord1(int client_id);
 	
 }
-
