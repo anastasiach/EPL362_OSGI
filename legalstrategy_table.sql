b1cae0e458c3a37d45677562d8ec9f0d2b407bf0
mpashi01 <mpashi01@cs.ucy.ac.cy>
2018-04-30 18:27:03 +0300

    Delete legalstrategy_table.sql

--- legalstrategy_table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `legalstrategy` (
-  `LegalStrategyID` int(11) NOT NULL,
-  `Name` varchar(45) NOT NULL,
-  `Side-effects` varchar(1000) NOT NULL,
-  PRIMARY KEY (`LegalStrategyID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
