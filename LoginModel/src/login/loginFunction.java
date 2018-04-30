d8219c5ead83e08e9fcb63681f07237beb08961b
asavvii02 <34001348+asavvii02@users.noreply.github.com>
2018-04-30 18:52:42 +0300

    Add files via upload

---
+++ LoginModel/src/login/loginFunction.java
@@ -0,0 +1,16 @@
+package login;
+/**
+ * 
+ * @author  Anastasia Chimona
+ * 			Anastasia Yiannaki
+ *			Antonia Savvia
+ *			Marina Pashiali
+ *
+ *		declaration of the login function
+ */
+public interface loginFunction {
+	
+	int login(String username, String password);
+	
+
+}
